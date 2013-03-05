bae9bb7d17a50ea38b56f03237acf2e0d154d52b
rsa <kris13@bk.ru>
2013-03-05 11:44:59 +0400

    use const reference instead of ObjectGuid copy in some Map search operations. must some speedup code. TESTING.

--- src/game/Map.cpp
+++ src/game/Map.cpp
@@ -2004,7 +2004,7 @@ void Map::EraseObject(WorldObject* object)
     EraseObject(object->GetObjectGuid());
 }
 
-void Map::EraseObject(ObjectGuid guid)
+void Map::EraseObject(ObjectGuid const& guid)
 {
     if (guid.IsEmpty())
         return;
@@ -2013,7 +2013,7 @@ void Map::EraseObject(ObjectGuid guid)
     m_objectsStore.erase(guid);
 }
 
-WorldObject* Map::FindObject(ObjectGuid guid)
+WorldObject* Map::FindObject(ObjectGuid const& guid)
 {
     if (guid.IsEmpty())
         return NULL;
@@ -2023,8 +2023,6 @@ WorldObject* Map::FindObject(ObjectGuid guid)
     return (itr == m_objectsStore.end()) ? NULL : itr->second;
 }
 
-
-
 /**
  * Function return player that in world at CURRENT map
  *
@@ -2033,7 +2031,7 @@ WorldObject* Map::FindObject(ObjectGuid guid)
  *
  * @param guid must be player guid (HIGHGUID_PLAYER)
  */
-Player* Map::GetPlayer(ObjectGuid guid, bool globalSearch)
+Player* Map::GetPlayer(ObjectGuid const& guid, bool globalSearch)
 {
     if (globalSearch)                                           // possible obsolete
     {
@@ -2049,7 +2047,7 @@ Player* Map::GetPlayer(ObjectGuid guid, bool globalSearch)
  *
  * @param guid must be creature or vehicle guid (HIGHGUID_UNIT HIGHGUID_VEHICLE)
  */
-Creature* Map::GetCreature(ObjectGuid guid)
+Creature* Map::GetCreature(ObjectGuid const& guid)
 {
     return (Creature*)FindObject(guid);
 }
@@ -2059,7 +2057,7 @@ Creature* Map::GetCreature(ObjectGuid guid)
  *
  * @param guid must be pet guid (HIGHGUID_PET)
  */
-Pet* Map::GetPet(ObjectGuid guid)
+Pet* Map::GetPet(ObjectGuid const& guid)
 {
     return (Pet*)FindObject(guid);
 }
@@ -2071,9 +2069,9 @@ Pet* Map::GetPet(ObjectGuid guid)
  *
  * @param guid must be corpse guid (HIGHGUID_CORPSE)
  */
-Corpse* Map::GetCorpse(ObjectGuid guid)
+Corpse* Map::GetCorpse(ObjectGuid const& guid)
 {
-    Corpse * ret = ObjectAccessor::GetCorpseInMap(guid,GetId());
+    Corpse * ret = ObjectAccessor::GetCorpseInMap(guid, GetId());
     return ret && ret->GetInstanceId() == GetInstanceId() ? ret : NULL;
 }
 
@@ -2082,7 +2080,7 @@ Corpse* Map::GetCorpse(ObjectGuid guid)
  *
  * @param guid must be non-player unit guid (HIGHGUID_PET HIGHGUID_UNIT HIGHGUID_VEHICLE)
  */
-Creature* Map::GetAnyTypeCreature(ObjectGuid guid)
+Creature* Map::GetAnyTypeCreature(ObjectGuid const& guid)
 {
     switch (guid.GetHigh())
     {
@@ -2101,7 +2099,7 @@ Creature* Map::GetAnyTypeCreature(ObjectGuid guid)
  *
  * @param guid must be gameobject guid (HIGHGUID_GAMEOBJECT)
  */
-GameObject* Map::GetGameObject(ObjectGuid guid)
+GameObject* Map::GetGameObject(ObjectGuid const& guid)
 {
     return (GameObject*)FindObject(guid);
 }
@@ -2111,7 +2109,7 @@ GameObject* Map::GetGameObject(ObjectGuid guid)
  *
  * @param guid must be dynamic object guid (HIGHGUID_DYNAMICOBJECT)
  */
-DynamicObject* Map::GetDynamicObject(ObjectGuid guid)
+DynamicObject* Map::GetDynamicObject(ObjectGuid const& guid)
 {
     return (DynamicObject*)FindObject(guid);
 }
@@ -2121,7 +2119,7 @@ DynamicObject* Map::GetDynamicObject(ObjectGuid guid)
  *
  * @param guid must be dynamic object guid (HIGHGUID_MO_TRANSPORT)
  */
-Transport* Map::GetTransport(ObjectGuid guid)
+Transport* Map::GetTransport(ObjectGuid const& guid)
 {
     return (Transport*)FindObject(guid);
 }
@@ -2134,7 +2132,7 @@ Transport* Map::GetTransport(ObjectGuid guid)
  *
  * @param guid must be unit guid (HIGHGUID_PLAYER HIGHGUID_PET HIGHGUID_UNIT HIGHGUID_VEHICLE)
  */
-Unit* Map::GetUnit(ObjectGuid guid)
+Unit* Map::GetUnit(ObjectGuid const& guid)
 {
     if (guid.IsPlayer())
         return GetPlayer(guid);
@@ -2145,7 +2143,7 @@ Unit* Map::GetUnit(ObjectGuid guid)
 /**
  * Function return world object in world at CURRENT map, so any except transports
  */
-WorldObject* Map::GetWorldObject(ObjectGuid guid)
+WorldObject* Map::GetWorldObject(ObjectGuid const& guid)
 {
     switch(guid.GetHigh())
     {
@@ -2416,7 +2414,7 @@ void Map::UpdateWorldState(uint32 state, uint32 value)
  * @param targetGuid (attackerGuid)
  */
 
-void Map::AddAttackerFor(ObjectGuid targetGuid, ObjectGuid attackerGuid)
+void Map::AddAttackerFor(ObjectGuid const& targetGuid, ObjectGuid const& attackerGuid)
 {
     if (targetGuid.IsEmpty() || attackerGuid.IsEmpty())
         return;
@@ -2434,7 +2432,7 @@ void Map::AddAttackerFor(ObjectGuid targetGuid, ObjectGuid attackerGuid)
     }
 }
 
-void Map::RemoveAttackerFor(ObjectGuid targetGuid, ObjectGuid attackerGuid)
+void Map::RemoveAttackerFor(ObjectGuid const& targetGuid, ObjectGuid const& attackerGuid)
 {
     if (targetGuid.IsEmpty() || attackerGuid.IsEmpty())
         return;
@@ -2447,7 +2445,7 @@ void Map::RemoveAttackerFor(ObjectGuid targetGuid, ObjectGuid attackerGuid)
     }
 }
 
-void Map::RemoveAllAttackersFor(ObjectGuid targetGuid)
+void Map::RemoveAllAttackersFor(ObjectGuid const& targetGuid)
 {
     if (targetGuid.IsEmpty())
         return;
@@ -2460,13 +2458,13 @@ void Map::RemoveAllAttackersFor(ObjectGuid targetGuid)
     }
 }
 
-GuidSet& Map::GetAttackersFor(ObjectGuid targetGuid)
+GuidSet& Map::GetAttackersFor(ObjectGuid const& targetGuid)
 {
     ReadGuard Guard(GetLock());
     return m_attackersMap[targetGuid];
 }
 
-void Map::CreateAttackersStorageFor(ObjectGuid targetGuid)
+void Map::CreateAttackersStorageFor(ObjectGuid const& targetGuid)
 {
     if (targetGuid.IsEmpty())
         return;
@@ -2479,7 +2477,7 @@ void Map::CreateAttackersStorageFor(ObjectGuid targetGuid)
 
 }
 
-void Map::RemoveAttackersStorageFor(ObjectGuid targetGuid)
+void Map::RemoveAttackersStorageFor(ObjectGuid const& targetGuid)
 {
     if (targetGuid.IsEmpty())
         return;
